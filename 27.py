b587b3b551fc7ba1dda6d6cf6053002c11f63ef1
nicolasitpereira <nicolas.pereira.it@gmail.com>
2024-09-27 22:38:33 -0300

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,4 +1,4 @@
-print(" [ 1 ] SOMA \n [ 2 ] SUBTRAÇÃO \n [ 3 ] MULTIPLICAÇÃO \n [ 4 ] DIVISÃO \n [ 6 ] EXPONECIAÇÃO \n [ 7 ] SAIR")
+print(" [ 1 ] SOMA \n [ 2 ] SUBTRAÇÃO \n [ 3 ] MULTIPLICAÇÃO \n [ 4 ] DIVISÃO \n [ 5 ] EXPONENCIAÇÃO \n [ 6 ] SAIR")
 option = int(input("SUA OPÇÃO : "))
 
 
@@ -6,15 +6,40 @@ while option != 7:
     match option:
         case 1:
             print("SOMA")
-            numero1 = int(input("Digite o primeiro nmumero para SOMA : "))
-            numero2 = int(input("DIgite outro numero para SOMA : "))
+            numero1 = float(input("Digite o primeiro numero : "))
+            numero2 = float(input("DIgite outro numero : "))
             resultado = numero1 + numero2
             print(f"O resultado da soma é : {resultado}")
             break
         case 2:
             print("SUBTRAIR")
-            numero1 = int(input("Digite o primeiro nmumero para SUBTRAIR : "))
-            numero2 = int(input("DIgite outro numero para SUBTRAIR : "))
+            numero1 = float(input("Digite o primeiro nmumero : "))
+            numero2 = float(input("DIgite outro numero : "))
             resultado = numero1 - numero2
             print(f"O resultado da SUBTRAÇÃO é : {resultado}")
             break
+        case 3:
+            print("MULTIPLICAÇÃO")
+            numero1 = float(input("Digite p primeiro numero : "))
+            numero2 = float(input("Digite outro numero : "))
+            resultado = numero1 * numero2
+            print(f"O reesultado da MULTIPLICAÇÃO é : {resultado}")
+            break
+        case 4:
+            print("DIVISÃO")
+            numero1 = float(input("Digite p primeiro numero :"))
+            numero2 = float(input("Digite outro numero : "))
+            resultado = numero1 / numero2
+            print(f"O reesultado da DIVISÃO é : {resultado}")
+            break
+        case 5:
+            print("EXPONENCIAÇÃO")
+            numero1 = float(input("Digite o primeiro numero : "))
+            numero2 = float(input("Digite outro numero : "))
+            resultado = numero1 ** numero2
+            print(f"O resultado da EXPONENCIAÇÃO é : {resultado}")
+            break
+        case 6:
+            print("SAINDO . . .")
+            break
+
