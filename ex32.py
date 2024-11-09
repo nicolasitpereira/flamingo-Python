65648183f952f1928fc85c3c38f079f347f97fd5
nicolasitpereira <nicolas.pereira.it@gmail.com>
2024-11-09 08:47:22 -0300

    Create ex32.py

---
+++ ex32.py
@@ -0,0 +1,8 @@
+# interseção entre Sets
+
+set1 = {"NICOLAS", "BRUNA", "TALISSA", "SAMUEL", "GUILHERME"}
+set2 = {"BRUNA", "SAMUEL", "DIEGO", "LAYLA"}
+
+intersection = set1 & set2
+
+print("Interseção entre set1 e set2:", intersection)
